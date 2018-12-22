030c5a91432d0c1717a2afd58a23586a1fdf30a2
lincolnbphillips001 <37947122+lincolnbphillips001@users.noreply.github.com>
2018-12-22 21:59:14 +1000

    Updated game.h

--- game.h
+++ game.h
@@ -1,7 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <goocanvas.h>
+#include <gtk/gtk.h>
+
+const gboolean GAME_OVER = 0;
+const gboolean GAME_RUNNING = 1;
 
 class Game {
 
@@ -12,20 +15,21 @@ class Game {
 	~Game (void);
 
 	// Member functions definitions
-	void get_game_info (void);
-	void set_current_board (gint **);
+	void set_current_board (guint **);
+	void reset_current_board ();
 	void display_board (void);
 	void set_game_status (gboolean);
 	gboolean get_game_status (void);
 	void reset_number_of_moves (void);
+	guint get_number_of_moves (void);
 	void increment_number_of_moves (void);
-	gint check_board_for_win (void);
+	guint check_board_for_win (void);
 
 	private:
 	
-	gint **gCurrentTicTacToeBoard;
+	guint **gCurrentTicTacToeBoard;
 	gboolean gGameStatus;
-	gint gNumberOfMoves;
+	guint gNumberOfMoves;
 
 };
 
