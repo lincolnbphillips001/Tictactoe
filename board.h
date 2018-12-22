99e24ebcecc956a2d8ec064f708b988508ab6450
lincolnbphillips001 <37947122+lincolnbphillips001@users.noreply.github.com>
2018-12-22 21:58:24 +1000

    Updated board.h

--- board.h
+++ board.h
@@ -1,33 +1,55 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include <goocanvas.h>
+#include <gtk/gtk.h>
+
+const guint BOARD_NOT_FULL = 0;
+const guint BOARD_IS_FULL = 1;
+const guint NUMBER_OF_BOXES = 9;
+const guint ROW_1 = 1;
+const guint ROW_2 = 2;
+const guint ROW_3 = 3;
+const guint COLUMN_1 = 1;
+const guint COLUMN_2 = 2;
+const guint COLUMN_3 = 3;
+const guint NUMBER_OF_ROWS = 3;
+const guint NUMBER_OF_COLUMNS = 3;
+const guint BOX_1 = 1;
+const guint BOX_2 = 2;
+const guint BOX_3 = 3;
+const guint BOX_4 = 4;
+const guint BOX_5 = 5;
+const guint BOX_6 = 6;
+const guint BOX_7 = 7;
+const guint BOX_8 = 8;
+const guint BOX_9 = 9;
+const guint BOX_NEVER_CLICKED = 0;
+const guint BOX_CLICKED_ALREADY = 1;
 
 class Board {
   
 	public:
 
 	// This is the constructor
-	Board (gint);
+	Board (guint);
 	// This is the destructor
 	~Board (void);
 
 	// Member functions definitions
-	void get_board_info (void);
-	void set_box_clicked (gint);
-	gboolean get_box_clicked (gint);
-	void update_board (gint, gint);
+	void set_box_clicked (guint);
+	gboolean get_box_clicked (guint);
+	void update_board (guint, guint);
 	void display_board (void);
 	gboolean is_board_empty(void);
 	gboolean is_board_full(void);
-	gint** get_current_board (void);
+	guint** get_current_board (void);
 	void reset_board (void);
 
 	private:
 
-	gint gNumOfBoxes;
+	guint gNumOfBoxes;
 	gboolean *gBoxClicked;
-	gint **gTicTacToeBoard;
+	guint **gTicTacToeBoard;
 
 };
 
