072d028970ec47193989b518d225309ebe1ba3b4
lincolnbphillips001 <37947122+lincolnbphillips001@users.noreply.github.com>
2018-12-22 22:02:11 +1000

    Updated player.h

--- player.h
+++ player.h
@@ -1,28 +1,43 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <goocanvas.h>
+#include <gtk/gtk.h>
+
+const gint NUMBER_OF_PLAYERS = 2;
+const gint PLAYER_1 = 0;
+const gint PLAYER_2 = 1;
+const gint NO_PLAYER_WIN = 0;
+const gint PLAYER_1_WIN = 1;
+const gint PLAYER_2_WIN = 2;
+
+typedef struct PlayerMoveRecord {
+	guint player;
+	guint boxSelected;
+} PlayerMoveRecord;
 
 class Player {
   
 	public:
 
 	// This is the constructor
-	Player (gint, gint);
+	Player (guint, guint);
 	// This is the destructor
 	~Player (void);
 
 	// Member functions definitions
-	void get_player_info (void);
-	void set_current_player (gint);
-	gint get_current_player (void);
+	void set_current_player (guint);
+	guint get_current_player (void);
 	void switch_player (void);
+	void record_player_move (guint, guint, guint);
+	PlayerMoveRecord* get_player_moves (void);
+	void reset_player_moves (void);
 
 	private:
 
-	gint gNumberOfPlayers;
-	gint gCurrentPlayer;
-
+	guint gNumberOfPlayers;
+	guint gCurrentPlayer;
+	PlayerMoveRecord *gPlayerMoveRecord;
+	guint gPlayerMoveRecordIndex;
 };
 
 #endif //PLAYER_H
