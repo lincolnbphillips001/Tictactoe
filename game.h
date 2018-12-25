687ed94058238d6f2ca65c95ccab7f8dea23ea0f
lincolnbphillips001 <37947122+lincolnbphillips001@users.noreply.github.com>
2018-12-25 14:21:52 +1000

    Added state for win line drawing

--- game.h
+++ game.h
@@ -6,6 +6,20 @@
 const gboolean GAME_OVER = 0;
 const gboolean GAME_RUNNING = 1;
 
+const guint NO_CURRENT_WIN = 0;
+const guint TOP_ROW = 1;
+const guint MIDDLE_ROW = 2;
+const guint BOTTOM_ROW = 3;
+const guint LEFT_COLUMN = 4;
+const guint MIDDLE_COLUMN = 5;
+const guint RIGHT_COLUMN = 6;
+const guint TOP_LEFT_TO_BOTTOM_RIGHT = 7;
+const guint TOP_RIGHT_TO_BOTTOM_LEFT = 8;
+
+const guint NO_WIN = 0;
+const guint PLAYER_1_NUMBER = 1;
+const guint PLAYER_2_NUMBER = 2;
+
 class Game {
 
    public:
@@ -24,12 +38,15 @@ class Game {
 	guint get_number_of_moves (void);
 	void increment_number_of_moves (void);
 	guint check_board_for_win (void);
+	guint get_win_direction (void);
+	void reset_win_direction (void);
 
 	private:
 	
 	guint **gCurrentTicTacToeBoard;
 	gboolean gGameStatus;
 	guint gNumberOfMoves;
+	guint gDirectionOfWin;
 
 };
 
